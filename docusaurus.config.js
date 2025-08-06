9f565596deff91e84f088d2903755eeac5212398
Rami Mouro <ramimouro@gmail.com>
2025-08-06 17:00:03 -0600

    removed extra file, shifted run sample applications, added graphic

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -151,6 +151,11 @@ const config = {
         darkTheme: prismThemes.dracula,
       },
     }),
+
+  markdown: {
+    mermaid: true,
+  },
+  themes: ['@docusaurus/theme-mermaid'],
 };
 
 export default config;
